a332d26d79479cba206ad827a7042ca83d1bbcab
ramon-richard <ramon.richard@iisgalvanimi.edu.it>
2022-01-08 14:42:22 +0000

    labels inutili e spam

---
+++ prepVerificaReddit/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'prepVerificaReddit';
+  articles = new Array<string>(); //Dichiaro un array di stringhe
+}
